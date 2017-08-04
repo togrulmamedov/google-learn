0950f4eb28c471c30d1a08ab8534a8fcce9f95d3
t.mamedov <t.mamedov@prismsolutions.ru>
2017-08-04 18:13:16 +0300

    Initial local commit [master]

---
+++ app/src/main/java/com/aspire/tohrul/googlelearn/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aspire.tohrul.googlelearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+ * This is the main activity (the entry point for your app).
+ * When you build and run the app, the system launches an
+ * instance of this Activity and loads its layout.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
